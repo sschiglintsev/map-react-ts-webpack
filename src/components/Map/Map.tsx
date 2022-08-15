9e32cc42e3c64e55ec22ff42c8b7d731ad01e364
sschiglintsev <s.s.chiglintsev@gmail.com>
2022-08-15 13:06:12 +0500

    add: module Map Yandex and component Map

---
+++ src/components/Map/Map.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import style from './Map.module.css';
+
+declare var ymaps: any;
+
+export const Map = () => {
+
+    ymaps.ready(init);
+
+    function init() {
+        const myMap = new ymaps.Map("map", {
+            center: [55.76, 37.64],
+            zoom: 20
+        });
+
+        myMap.geoObjects
+            .add(new ymaps.Placemark([55.76, 37.64], {
+                balloonContent: 'цвет <strong>носика Гены</strong>',
+                iconCaption: 'Очень длиннный, но невероятно интересный текст'
+            }, {
+                preset: 'islands#greenDotIconWithCaption'
+            }))
+
+    }
+
+    return (
+        <>
+            <div id="map" className={style.map}/>
+        </>
+    );
+};
